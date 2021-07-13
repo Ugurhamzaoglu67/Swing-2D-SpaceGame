d2b120a92a40b8b645b322eb09acf77b742c5eeb
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-07-13 15:35:57 +0300

    Opened Game Screen, Created SpaceX Ship and fireball

--- src/spacegame/MainGameScreen.java
+++ src/spacegame/MainGameScreen.java
@@ -15,6 +15,24 @@ public class MainGameScreen  extends JFrame{
     
     public static void main(String[] args) {
         MainGameScreen mainGameScreen = new MainGameScreen("***Space Game***");
+       
+        mainGameScreen.setResizable(false);
+        mainGameScreen.setFocusable(false);
+        mainGameScreen.setSize(900,700);
+        mainGameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        MyGame game = new MyGame();
+        
+        game.requestFocus(); // Odağı bana ver. Klavyeden işlemler için.
+        
+        game.addKeyListener(game);  
+        game.setFocusable(true);    // Odak JPanel'de.
+        game.setFocusTraversalKeysEnabled(false); //Klave işlemleri gerçekleşmesi için
+        
+        //JPanel add -> JFrame
+        mainGameScreen.add(game);
+        mainGameScreen.setVisible(true);
         
     }
 }
---
+++ src/spacegame/MyGame.java
@@ -0,0 +1,124 @@
+
+package spacegame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageInputStream;
+import javax.swing.JPanel;
+
+
+// FIRE CLASS 
+class Fire {
+    private int x,y;
+
+    public Fire(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
+
+
+
+
+public class MyGame extends JPanel implements KeyListener, ActionListener{
+
+    private int passingTime = 0;
+    private int countFire = 0;
+    private BufferedImage myimage;
+    
+    private ArrayList<Fire> fires = new ArrayList<>();
+    
+    private int fireY = 1;
+    private int ballX = 0;
+    private int ballDirectionX = 2;
+    
+    //SPACE SHIP
+    private int spaceX = 0;
+    private int directionSpaceX = 20;
+    
+    
+    
+    // CONSTRUCTOR
+    public MyGame(){
+        
+        try {
+            myimage = ImageIO.read(new FileImageInputStream(new File("spaceship-png-icon-17260.png")));
+        } catch (IOException ex) {
+            Logger.getLogger(MyGame.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+
+        setBackground(Color.BLACK);
+        
+        
+        
+    }
+
+   // FIRE BALL
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g); 
+        
+        g.setColor(Color.red);
+        g.fillOval(ballX, 0, 20, 20);
+        g.drawImage(myimage, spaceX, 550, myimage.getWidth()/4, myimage.getHeight()/4, this);
+    }
+
+    @Override
+    public void repaint() {
+        super.repaint(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    // KEYBOARD OPERATIONS
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    
+}
